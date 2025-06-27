4701738746d8078e18b88b9bb6f55362c62062ea
ryobotchan525 <ryo.yanai0525@gmail.com>
2025-06-28 03:28:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -53,5 +53,5 @@ def healthcheck():
     return "Bot is alive", 200
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
